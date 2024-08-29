11f24f1ac9f6fb8833822690469f5289d67ae5ed
zoolotoui <chag.med.amine@gmail.com>
2024-08-29 14:43:36 +0100

    stat: firing up

---
+++ EventKeeper.java
@@ -0,0 +1,11 @@
+public class EventKeeper {
+    public static void main(String[] args) throws InterruptedException{
+        System.out.println("****************************************************************************************************************************************************************");
+        Thread.sleep(1000);
+        System.out.println("H-E-Y !");
+        Thread.sleep(1000);
+        System.out.println("B-Y-E!");
+
+        
+    }
+}
