3c30d39c7aee2a4d225b067cf7e6903bc2af33a1
zoolotoui <chag.med.amine@gmail.com>
2024-08-30 16:32:43 +0100

    stat: crating entities

--- src/entity/Inscription.java
+++ src/entity/Inscription.java
@@ -1,3 +1,12 @@
 public class Inscription {
+    private id;
+
+    public Inscription(int id){
+        this.id = id;
+    }
+
+    public int GetId(){
+        return this.id;
+    }
     
 }
--- src/entity/User.java
+++ src/entity/User.java
@@ -2,11 +2,13 @@ public class User {
     
     private  Int Id;
     private String name;
+    private String password;
     private boolean role;
 
     public User(Int Id,String name,boolean role){
         this.Id = Id;
         this.name = name;
+        this.password = password;
         this.role = role;
 
     }
@@ -21,9 +23,15 @@ public class User {
     public boolean GetRole(){
         return this.role;
     }
+    public String GetPassword(){
+        return this.password;
+    }
 
     public SetName(String name){
         this.name =  name;
+    }
+    public SetPassword(String password){
+        this.password =  password;
     }
     public SetRole(boolean role){
         this.role =  role;
