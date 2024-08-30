9e8ef06a132976b3266bbe8c25286d3a8932d047
zoolotoui <chag.med.amine@gmail.com>
2024-08-30 12:19:35 +0100

    stat: refactoring

--- EventKeeper.java
+++ EventKeeper.java
@@ -6,30 +6,29 @@ public class EventKeeper {
         do{
 
             System.out.println("====MAIN MENU====");
-            System.out.println("[1]. Option 1: Gret");
-            System.out.println("[2]. Option 2: Addition");
+            System.out.println("[1]. Option 1: Admin");
+            System.out.println("[2]. Option 2: Participant");
             System.out.println("[3]. Option 3: Exit");
             System.out.println("=======================");
             Thread.sleep(100);
-            System.out.println("ENTER YOUR CHOICE: ");
+            System.out.println("CHOSE YOUR ROLE: ");
 
             choice = scanner.nextInt();
             switch (choice) {
                 case 1:
-                System.out.print("\u001B[2J\u001B[0;0H");
-                
-                Greet(scanner);
-                break;
+                    System.out.print("\u001B[2J\u001B[0;0H");                
+                    AdminMenu(scanner);
+                    
                 case 2:
-                System.out.print("\u001B[2J\u001B[0;0H");
-                Addition(scanner);
-                break;
+                    System.out.print("\u001B[2J\u001B[0;0H");
+                    // Addition(scanner);
+                    break;
                 case 3:
-                System.out.print("\u001B[2J\u001B[0;0H");
-                System.out.println("Exiting the program. Goodbye!");
+                    System.out.print("\u001B[2J\u001B[0;0H");
+                    System.out.println("Exiting the program. Goodbye!");
                     break;
             
-                    default:
+                default:
                     System.out.println("Invalid choice. Please try again.");
                 }
                 
@@ -42,9 +41,9 @@ public class EventKeeper {
     }
 
 
-    private static void Greet(Scanner scanner){
+    private static void Admin(Scanner scanner){
         scanner.nextLine();
-        System.out.println("\n\n====ENTER YOUR NAME: ");
+        System.out.println("====ENTER YOUR NAME: ");
         String name = scanner.nextLine();
         System.out.println("\n====Hello " + name.toUpperCase() + " Welcome to the app");
 
@@ -59,6 +58,27 @@ public class EventKeeper {
         System.out.println("====THE SUM OF YOUR NUMBERS IS: "+ sum );
         
 
+    }
+    public static int AdminMenu (Scanner scanner) throws InterruptedException{
+        
+            
+            System.out.println("\n\n====ADMIN MENU LOADING");
+            Thread.sleep(500);
+            System.out.println("[1] Option : Event's crud");
+            Thread.sleep(500);
+            System.out.println("[2] Option : Participant crud");
+            Thread.sleep(500);
+            System.out.println("[3] Option : Show events");
+            Thread.sleep(500);
+            System.out.println("[4] Option : Show inscriptions");
+            Thread.sleep(500);
+            System.out.println("====CHOOSE AN OPTION:");            
+            int choice = scanner.nextInt();
+            return choice;
+        }
+
+    public static void AddEvent(Scanner  scanner){
+        
     }
 
     
